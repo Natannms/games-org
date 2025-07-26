df5c9707984385c2a2d6a31f3207ab7c1679693b
Natan Martins <agnusnat.nms@gmail.com>
2025-07-26 18:33:10 +0000

    quando side menu estiver comprimido deve exibir somente os icones, quand

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -92,7 +92,7 @@ export default function DashboardLayout({
         {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
                 <Link href={item.href}>
-                    <SidebarMenuButton isActive={pathname === item.href}>
+                    <SidebarMenuButton isActive={pathname === item.href} tooltip={item.label}>
                         <item.icon />
                         <span>{item.label}</span>
                     </SidebarMenuButton>
