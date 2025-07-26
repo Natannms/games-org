1e1c7f09acbeff9b5adc3bed6920083e81f30796
Natan Martins <agnusnat.nms@gmail.com>
2025-07-26 18:31:41 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -91,7 +91,7 @@ export default function DashboardLayout({
     <SidebarMenu>
         {navItems.map((item) => (
             <SidebarMenuItem key={item.href}>
-                <Link href={item.href} legacyBehavior passHref>
+                <Link href={item.href}>
                     <SidebarMenuButton isActive={pathname === item.href}>
                         <item.icon />
                         <span>{item.label}</span>
